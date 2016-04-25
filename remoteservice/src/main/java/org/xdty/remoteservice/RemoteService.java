4b7ae4de3b6eaed3fb9475c28a7185b53dcf1d5c
tianyu <xdtianyu@gmail.com>
2016-04-25 21:58:30 +0800

    add service

---
+++ remoteservice/src/main/java/org/xdty/remoteservice/RemoteService.java
@@ -0,0 +1,31 @@
+package org.xdty.remoteservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class RemoteService extends Service {
+    private static final String TAG = RemoteService.class.getSimpleName();
+    private IBinder mBinder = new IRemoteService.Stub() {
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
+                double aDouble, String aString) throws RemoteException {
+            Log.d(TAG, "basicTypes: ");
+        }
+
+        @Override
+        public void addUser(User user) throws RemoteException {
+            Log.d(TAG, "addUser: ");
+        }
+    };
+
+    public RemoteService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
